3a24d0b14e324dbc1a95038bcd23e8873211ef30
Teo <balanced-tree@pm.me>
2026-07-16 15:29:33 +0200

    chore: natspec

--- src/SupplyBorrowVault.sol
+++ src/SupplyBorrowVault.sol
@@ -3,6 +3,10 @@ pragma solidity 0.8.30;
 
 import {ISupplyBorrowVault} from "./interfaces/ISupplyBorrowVault.sol";
 
+/// @title SupplyBorrowVault
+/// @author balanced-tree
+/// @notice Vault that deposits into Aave v4 Spoke as strategy, borrows against supplied assets and uses the borrowed assets to deposit into another vault.
+/// @dev Has synchronous deposits and asynchronous redemptions.
 contract SupplyBorrowVault {
 
 }
