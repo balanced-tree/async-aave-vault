61044491a34a1204b327ba1fe0ba2bc3783ffad4
Teo <balanced-tree@pm.me>
2026-07-21 16:02:33 +0200

    feat: add contract

---
+++ test/TestBase.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.30;
+
+import {Test} from "forge-std/Test.sol";
+import {IERC20Metadata} from "openzeppelin/token/ERC20/extensions/IERC20Metadata.sol";
+
+abstract contract TestBase is Test {
+
+}
