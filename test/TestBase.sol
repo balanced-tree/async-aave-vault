ca8e1942ba490b3266e4e61fe2e679eed43c76c1
Teo <balanced-tree@pm.me>
2026-07-21 16:03:41 +0200

    chore: add chain constants

--- test/TestBase.sol
+++ test/TestBase.sol
@@ -5,5 +5,41 @@ import {Test} from "forge-std/Test.sol";
 import {IERC20Metadata} from "openzeppelin/token/ERC20/extensions/IERC20Metadata.sol";
 
 abstract contract TestBase is Test {
+    // Test Accounts
+    address public admin;
+    address public alice;
+    address public bob;
+    address public eve;
+
+    address public manager;
+    address public treasury;
+
+    // Chains
+    string public constant ETHEREUM_KEY = "Ethereum";
+    string public constant BASE_KEY = "Base";
+
+    uint64 public constant ETH = 1;
+    uint64 public constant BASE = 8453;
+
+    // RPC
+    string public constant ETHEREUM_RPC_URL_KEY = "ETHEREUM_RPC_URL";
+    string public constant BASE_RPC_URL_KEY = "BASE_RPC_URL";
+
+    mapping(uint64 chainId => uint256 fork) public forks;
+    // forge-lint: disable-next-line(mixed-case-variable)
+    mapping(uint64 chainId => string forkUrl) public rpcURLs;
+    uint64[] public chainIds = [ETH, BASE];
+
+    string public ethereumRpcUrl = vm.envString(ETHEREUM_RPC_URL_KEY);
+    string public baseRpcUrl = vm.envString(BASE_RPC_URL_KEY);
+
+    // Tokens
+    string public constant GHO_KEY = "GHO";
+    string public constant USDC_KEY = "USDC";
+    string public constant USDT_KEY = "USDT";
+    string public constant WETH_KEY = "WETH";
+    string public constant WBTC_KEY = "WBTC";
+
+    
 
 }
