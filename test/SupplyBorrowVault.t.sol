d186dd1969d0d5e4a573fd14570f3c61fb023e4c
Teo <balanced-tree@pm.me>
2026-07-22 15:59:47 +0200

    chore: imports

--- test/SupplyBorrowVault.t.sol
+++ test/SupplyBorrowVault.t.sol
@@ -1,7 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.30;
 
+// Internal
 import {TestBase} from "./TestBase.sol";
+import {SupplyBorrowVault} from "../src/SupplyBorrowVault.sol";
+import {ISupplyBorrowVault} from "../src/interfaces/ISupplyBorrowVault.sol";
 
 contract SupplyBorrowVaultTest is TestBase {
 
