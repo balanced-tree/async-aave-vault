b1a3166861a4971988b5c1e86cf4c1debc0166a0
Teo <balanced-tree@pm.me>
2026-07-27 09:14:02 +0200

    chore: update errors

--- src/interfaces/ISupplyBorrowVault.sol
+++ src/interfaces/ISupplyBorrowVault.sol
@@ -26,6 +26,7 @@ interface ISupplyBorrowVault is IERC4626, IERC7540Redeem {
     error INVALID_MANAGER();
     error NOT_IMPLEMENTED();
     error INVALID_OPERATOR();
+    error MAX_MINT_EXCEEDED();
     error INVALID_FEE_AMOUNT();
     error MAX_DEPOSIT_EXCEEDED();
 
