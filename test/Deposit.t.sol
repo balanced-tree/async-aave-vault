6a92e6fbca7e15bf6febb741335e47422beb54bf
Teo <balanced-tree@pm.me>
2026-08-31 10:02:48 +0200

    chore: add file

---
+++ test/Deposit.t.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.30;
+
+// Internal
+import {TestBase} from "./utils/TestBase.sol";
+import {SupplyBorrowVault} from "../src/SupplyBorrowVault.sol";
+import {ISupplyBorrowVault} from "../src/interfaces/ISupplyBorrowVault.sol";
+
+import {IERC20} from "openzeppelin/interfaces/IERC20.sol";
+import {SafeERC20} from "openzeppelin/token/ERC20/utils/SafeERC20.sol";
+import {ISpoke, ReserveFlags} from "aave-v4/spoke/interfaces/ISpoke.sol";
+
+contract DepositTest is TestBase {
+    using SafeERC20 for IERC20;
+
+    function setUp() public override {
+        super.setUp();
+    }
+}
