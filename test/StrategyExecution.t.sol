d706547acae88736c7acd4bbc0e4279ac0b88c37
Teo <balanced-tree@pm.me>
2026-09-08 08:52:05 +0200

    feat: revert case tests

--- test/StrategyExecution.t.sol
+++ test/StrategyExecution.t.sol
@@ -18,4 +18,54 @@ contract StrategyExecutionTest is TestBase {
     /*//////////////////////////////////////////////////////////////
                        EXECUTE STRATEGY TESTS
     //////////////////////////////////////////////////////////////*/
+    function test_ExecuteStrategy_revertsIfNotManager() public {
+        ISupplyBorrowVault.StrategyExecutionData memory strategy = ISupplyBorrowVault.StrategyExecutionData({
+            borrowAmount: 0,
+            depositAmount: 100e6,
+            minSharesRequired: 1
+        });
+
+        vm.prank(alice);
+        vm.expectRevert();
+        vault.executeStrategy(strategy);
+    }
+
+    function test_ExecuteStrategy_revertsOnZeroDepositAmount() public {
+        ISupplyBorrowVault.StrategyExecutionData memory strategy = ISupplyBorrowVault.StrategyExecutionData({
+            borrowAmount: 0,
+            depositAmount: 0,
+            minSharesRequired: 1
+        });
+
+        vm.prank(admin);
+        vm.expectRevert(ISupplyBorrowVault.ZERO_AMOUNT.selector);
+        vault.executeStrategy(strategy);
+    }
+
+    function test_ExecuteStrategy_revertsOnZeroMinShares() public {
+        ISupplyBorrowVault.StrategyExecutionData memory strategy = ISupplyBorrowVault.StrategyExecutionData({
+            borrowAmount: 0,
+            depositAmount: 100e6,
+            minSharesRequired: 0
+        });
+
+        vm.prank(admin);
+        vm.expectRevert(ISupplyBorrowVault.ZERO_AMOUNT.selector);
+        vault.executeStrategy(strategy);
+    }
+
+    function test_ExecuteStrategy_revertsOnInsufficientShares() public {
+        // Supply USDT as collateral, then demand impossibly many downstream shares
+        _depositAs(alice, 1000e6);
+
+        ISupplyBorrowVault.StrategyExecutionData memory strategy = ISupplyBorrowVault.StrategyExecutionData({
+            borrowAmount: 300e6,
+            depositAmount: 300e6,
+            minSharesRequired: type(uint256).max
+        });
+
+        vm.prank(admin);
+        vm.expectRevert(ISupplyBorrowVault.INSUFFICIENT_SHARES.selector);
+        vault.executeStrategy(strategy);
+    }
 }
