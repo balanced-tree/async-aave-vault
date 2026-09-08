cfd5c21c2c10db5d9a5aa09a58dc0bede06826f2
Teo <balanced-tree@pm.me>
2026-09-08 09:14:28 +0200

    chore: update imports

--- test/Deleverage.t.sol
+++ test/Deleverage.t.sol
@@ -5,5 +5,5 @@ import {TestBase} from "./utils/TestBase.sol";
 import {ISupplyBorrowVault} from "../src/interfaces/ISupplyBorrowVault.sol";
 
 import {IERC20} from "openzeppelin/interfaces/IERC20.sol";
-import {SafeERC20} from "openzeppelin/token/ERC20/utils/SafeERC20.sol";
 import {ISpoke} from "aave-v4/spoke/interfaces/ISpoke.sol";
+import {SafeERC20} from "openzeppelin/token/ERC20/utils/SafeERC20.sol";
