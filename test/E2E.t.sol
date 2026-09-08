7cc72fe83add1983fc7523d5a638b14fadadbb4a
Teo <balanced-tree@pm.me>
2026-09-08 09:12:55 +0200

    chore: update imports

--- test/E2E.t.sol
+++ test/E2E.t.sol
@@ -3,6 +3,10 @@ pragma solidity 0.8.30;
 
 // Internal
 import {TestBase} from "./utils/TestBase.sol";
+import {ISupplyBorrowVault} from "../src/interfaces/ISupplyBorrowVault.sol";
+
+import {IERC20} from "openzeppelin/interfaces/IERC20.sol";
+import {SafeERC20} from "openzeppelin/token/ERC20/utils/SafeERC20.sol";
 
 contract E2ETest is TestBase {
 
