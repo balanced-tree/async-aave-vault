847f0c28d67f93f286735a5c2a9fee4e991bee64
Teo <balanced-tree@pm.me>
2026-09-08 08:51:23 +0200

    chore: setup

---
+++ test/StrategyExecution.t.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.30;
+
+import {TestBase} from "./utils/TestBase.sol";
+import {ISupplyBorrowVault} from "../src/interfaces/ISupplyBorrowVault.sol";
+
+import {IERC20} from "openzeppelin/interfaces/IERC20.sol";
+import {SafeERC20} from "openzeppelin/token/ERC20/utils/SafeERC20.sol";
+import {ISpoke} from "aave-v4/spoke/interfaces/ISpoke.sol";
+
+contract StrategyExecutionTest is TestBase {
+    using SafeERC20 for IERC20;
+
+    function setUp() public override {
+        super.setUp();
+    }
+}
