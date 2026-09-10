a6452a64d1b42c8e21d522463177cd51ddbe71bb
Teo <balanced-tree@pm.me>
2026-09-10 09:14:11 +0200

    chore: add file

---
+++ test/Accounting.t.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.30;
+
+import {TestBase} from "./utils/TestBase.sol";
+import {ISupplyBorrowVault} from "../src/interfaces/ISupplyBorrowVault.sol";
+
+import {IERC20} from "openzeppelin/interfaces/IERC20.sol";
+import {ISpoke} from "aave-v4/spoke/interfaces/ISpoke.sol";
+import {SafeERC20} from "openzeppelin/token/ERC20/utils/SafeERC20.sol";
+
+contract AccountingTest is TestBase {
+    using SafeERC20 for IERC20;
+
+}
